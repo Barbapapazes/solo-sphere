63440ea17655c95fdad2f8fb6f5618f1166a605f
barbapapazes <e.soubiran25@gmail.com>
2023-10-12 18:07:40 +0200

    fix: update x padding in prose layout

--- layouts/prose.vue
+++ layouts/prose.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="mx-auto w-full prose dark:prose-invert prose-zinc">
+  <section class="px-4 mx-auto w-full prose dark:prose-invert prose-zinc">
     <slot />
   </section>
 </template>

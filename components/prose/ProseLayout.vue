fcc7925ddefdbab84982762f6030ec97019afebb
barbapapazes <e.soubiran25@gmail.com>
2023-11-12 16:11:27 +0100

    fix: remove UDivider

--- components/prose/ProseLayout.vue
+++ components/prose/ProseLayout.vue
@@ -27,7 +27,6 @@ defineProps<{
 
     <div class="max-w-prose mx-auto w-full row-start-2 xl:row-start-1 xl:row-span-2 xl:col-start-3 border-b border-b-zinc-200 dark:border-b-zinc-800 pb-6 xl:border-b-0 xl:pb-0">
       <ProseNav :toc="toc" :filename="filename" :resources="resources" class="xl:sticky xl:top-[5.5rem] xl:overflow-y-auto xl:max-h-[calc(100vh-5.5rem)]" />
-      <UDivider class="xl:hidden mt-6" />
     </div>
   </article>
 </template>
